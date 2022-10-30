ae828b046113093d5e90d5db4588304b7da6c981
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-30 21:26:01 +0530

    [sourav]add : flipCoin

---
+++ CsharpFunctionalProblem/CoinFlip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace CsharpFunctionalProblem
+{
+    internal class CoinFlip
+    {
+        Random random = new Random();
+        public double CoinToss()
+        {   
+            double number = random.NextDouble();
+            return number;
+        }
+        
+        public void FlipCoin()
+        {
+            double count = 0, headCount = 0, tailCount = 0, coinFlip;
+            Console.Write("Enter number of COIN flip for the toss : ");
+            coinFlip = Convert.ToInt32(Console.ReadLine());
+            while(coinFlip != count)
+            {
+                count++;
+                double toss = CoinToss();
+                if(toss < 0.5)
+                {
+                    tailCount++;
+                }
+                else
+                {
+                    headCount++;
+                }
+            }
+
+            double he = (double)((headCount * 100) / coinFlip);
+            double ta = (double)((tailCount * 100) / coinFlip);
+            Console.WriteLine("Winning percentage of HEAD is : " +he);
+            Console.WriteLine("Winning percentage of TAIL is : " +ta);
+
+        }
+    }
+}
---
+++ CsharpFunctionalProblem/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace CsharpFunctionalProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoinFlip fn = new CoinFlip();
+            fn.FlipCoin();
+        }
+    }
+}
+
