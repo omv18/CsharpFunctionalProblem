0305f110430e73964c9a46786e498a4a24c351a8
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-31 16:03:24 +0530

    [sourav]refactor : UC4

--- CsharpFunctionalProblem/Program.cs
+++ CsharpFunctionalProblem/Program.cs
@@ -5,14 +5,34 @@ namespace CsharpFunctionalProblem
     {
         static void Main(string[] args)
         {
-            CoinFlip fn = new CoinFlip();
-            fn.FlipCoin();
-            LeapYear lp = new LeapYear();
-            lp.CheckLeapYear();
-            Pow2N pow = new Pow2N();
-            pow.CalPow();
-            HarmonicNumber hm = new HarmonicNumber();
-            hm.CalHarmonic();
+            bool flag = true;
+            while (flag)
+            {
+                int val = Convert.ToInt32(Console.ReadLine());
+                switch (val)
+                {
+                    case 0:
+                        CoinFlip fn = new CoinFlip();
+                        fn.FlipCoin();
+                        break;
+                    case 1:
+                        LeapYear lp = new LeapYear();
+                        lp.CheckLeapYear();
+                        break;
+                    case 2:
+                        Pow2N pow = new Pow2N();
+                        pow.CalPow();
+                        break;
+                    case 3:
+                        HarmonicNumber hm = new HarmonicNumber();
+                        hm.CalHarmonic();
+                        break;
+                }
+            }
+
+
+
+
         }
     }
 }
