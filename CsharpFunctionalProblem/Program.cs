06730a7b111d0e6c6058b1f3c67610c0f93b8f71
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-31 16:41:28 +0530

    [sourav]add : Quo-Remainder

--- CsharpFunctionalProblem/Program.cs
+++ CsharpFunctionalProblem/Program.cs
@@ -11,21 +11,30 @@ namespace CsharpFunctionalProblem
                 int val = Convert.ToInt32(Console.ReadLine());
                 switch (val)
                 {
-                    case 0:
+                    case 1:
                         CoinFlip fn = new CoinFlip();
                         fn.FlipCoin();
                         break;
-                    case 1:
+                    case 2:
                         LeapYear lp = new LeapYear();
                         lp.CheckLeapYear();
                         break;
-                    case 2:
+                    case 3:
                         Pow2N pow = new Pow2N();
                         pow.CalPow();
                         break;
-                    case 3:
+                    case 4:
                         HarmonicNumber hm = new HarmonicNumber();
                         hm.CalHarmonic();
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        QuoRem quoRem = new QuoRem();
+                        quoRem.FindQuoRem();
+                        break;
+                    case 12:
+                        flag = false;
                         break;
                 }
             }
---
+++ CsharpFunctionalProblem/QuoRem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFunctionalProblem
+{
+    internal class QuoRem
+    {
+        public int EnterDivisor()
+        {
+            Console.Write("Enter the  Dividend value = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            return N;
+        }
+        public int EnterDividend()
+        {
+            Console.Write("Enter the Divisor value  = ");
+            int M = Convert.ToInt32(Console.ReadLine());
+            return M;
+        }
+        public void FindQuoRem()
+        {
+            int dividend = EnterDivisor();
+            int divisor = EnterDividend();
+            int quoitent = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Quoitent is : " + quoitent);
+            Console.WriteLine("Remainder is : " + remainder);
+
+        }
+    }
+}
